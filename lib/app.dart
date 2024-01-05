8f4c7063695eb131e0264020308f33c5025c7904
Shakib Absar <sabsar42@gmail.com>
2024-01-05 16:34:12 +0600

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:e_commerce_flutter_crafty_bay/presentation/ui/screens/splash_screens.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CraftyBay extends StatelessWidget {
+  const CraftyBay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: SplashScreen(
+
+      ),
+    );
+  }
+}
