fb935f034d85a7b2b0ec1b3f63dd19ccde6b3379
Shakib Absar <sabsar42@gmail.com>
2024-01-06 06:03:10 +0600

    countDown Timer OTP needs to be Implemented

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,7 @@ import 'package:get/get.dart';
 class CraftyBay extends StatelessWidget {
   const CraftyBay({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
